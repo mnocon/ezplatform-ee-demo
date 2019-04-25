d8293949f89a6618c2fc29138ddebe94e7f66480
ciastektk <tomasz.kryszan@ez.no>
2019-04-25 10:50:10 +0200

    DEMO-296: Refactor event subscribers for block rendering (#143)
    https://jira.ez.no/browse/DEMO-296

---
+++ src/AppBundle/Event/Subscriber/PlacesBlockSubscriber.php
@@ -0,0 +1,65 @@
+<?php
+
+/**
+ * @copyright Copyright (C) eZ Systems AS. All rights reserved.
+ * @license For full copyright and license information view LICENSE file distributed with this source code.
+ */
+declare(strict_types=1);
+
+namespace AppBundle\Event\Subscriber;
+
+use AppBundle\Event\AbstractBlockEvent;
+use AppBundle\Value\BlockPreRenderValueObject;
+use AppBundle\Value\BlockResponseValueObject;
+use EzSystems\EzPlatformPageFieldType\Event\BlockResponseEvent;
+use EzSystems\EzPlatformPageFieldType\Event\BlockResponseEvents;
+use EzSystems\EzPlatformPageFieldType\FieldType\Page\Block\Renderer\BlockRenderEvents;
+use EzSystems\EzPlatformPageFieldType\FieldType\Page\Block\Renderer\Event\PreRenderEvent;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+
+class PlacesBlockSubscriber extends AbstractBlockEvent implements EventSubscriberInterface
+{
+    private const BLOCK_IDENTIFIER = 'places';
+    private const CONTENT_TYPE_IDENTIFIER = 'place';
+
+    /**
+     * @return array The event names to listen to
+     */
+    public static function getSubscribedEvents(): array
+    {
+        return [
+            BlockRenderEvents::getBlockPreRenderEventName(self::BLOCK_IDENTIFIER) => 'onBlockPreRender',
+            BlockResponseEvents::getBlockResponseEventName(self::BLOCK_IDENTIFIER) => 'onBlockResponse',
+        ];
+    }
+
+    /**
+     * @param \EzSystems\EzPlatformPageFieldType\FieldType\Page\Block\Renderer\Event\PreRenderEvent $event
+     *
+     * @throws \eZ\Publish\API\Repository\Exceptions\InvalidArgumentException
+     * @throws \eZ\Publish\API\Repository\Exceptions\NotFoundException
+     * @throws \eZ\Publish\API\Repository\Exceptions\UnauthorizedException
+     */
+    public function onBlockPreRender(PreRenderEvent $event): void
+    {
+        $blockPreRender = new BlockPreRenderValueObject();
+        $blockPreRender->renderRequest = $event->getRenderRequest();
+        $blockPreRender->blockValue = $event->getBlockValue();
+        $blockPreRender->contentTypeIdentifier = self::CONTENT_TYPE_IDENTIFIER;
+
+        $this->setPreRenderParameters($blockPreRender);
+    }
+
+    /**
+     * @param \EzSystems\EzPlatformPageFieldType\Event\BlockResponseEvent $event
+     */
+    public function onBlockResponse(BlockResponseEvent $event): void
+    {
+        $blockResponse = new BlockResponseValueObject();
+        $blockResponse->response = $event->getResponse();
+        $blockResponse->blockValue = $event->getBlockValue();
+        $blockResponse->contentTypeIdentifier = self::CONTENT_TYPE_IDENTIFIER;
+
+        $this->processResponse($blockResponse);
+    }
+}
---
+++ src/AppBundle/Helper/LocationHelper.php
@@ -0,0 +1,62 @@
+<?php
+
+/**
+ * @copyright Copyright (C) eZ Systems AS. All rights reserved.
+ * @license For full copyright and license information view LICENSE file distributed with this source code.
+ */
+declare(strict_types=1);
+
+namespace AppBundle\Helper;
+
+use eZ\Publish\API\Repository\ContentService;
+use eZ\Publish\API\Repository\LocationService;
+use eZ\Publish\API\Repository\Values\Content\Location;
+
+class LocationHelper
+{
+    /** @var \eZ\Publish\API\Repository\ContentService */
+    private $contentService;
+
+    /** @var \eZ\Publish\API\Repository\LocationService */
+    private $locationService;
+
+    /**
+     * @param \eZ\Publish\API\Repository\ContentService $contentService
+     * @param \eZ\Publish\API\Repository\LocationService $locationService
+     */
+    public function __construct(ContentService $contentService, LocationService $locationService)
+    {
+        $this->contentService = $contentService;
+        $this->locationService = $locationService;
+    }
+
+    /**
+     * @param int $contentId
+     *
+     * @return \eZ\Publish\API\Repository\Values\Content\Location
+     *
+     * @throws \eZ\Publish\API\Repository\Exceptions\NotFoundException
+     * @throws \eZ\Publish\API\Repository\Exceptions\UnauthorizedException
+     */
+    public function loadLocationByContentId(int $contentId): Location
+    {
+        $contentInfo = $this->contentService->loadContentInfo($contentId);
+
+        return $this->locationService->loadLocation($contentInfo->mainLocationId);
+    }
+
+    /**
+     * @param int $contentId
+     *
+     * @return int
+     *
+     * @throws \eZ\Publish\API\Repository\Exceptions\NotFoundException
+     * @throws \eZ\Publish\API\Repository\Exceptions\UnauthorizedException
+     */
+    public function getLocationIdByContentId(int $contentId): int
+    {
+        $location = $this->loadLocationByContentId($contentId);
+
+        return $location->id;
+    }
+}
