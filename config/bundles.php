7236b408617a8d876f97e77d6ca48bcdc524e3d3
Damian Zabawa <damian.zabawa@ez.no>
2019-08-09 17:13:50 +0200

    Temporary solution for kaliop migration bundle

--- config/bundles.php
+++ config/bundles.php
@@ -60,5 +60,5 @@ return [
     EzSystems\EzPlatformGraphQL\EzSystemsEzPlatformGraphQLBundle::class => ['all' => true],
     Overblog\GraphQLBundle\OverblogGraphQLBundle::class => ['all' => true],
     Overblog\GraphiQLBundle\OverblogGraphiQLBundle::class => ['dev' => true],
-    Kaliop\eZMigrationBundle\EzMigrationBundle::class => ['all' => true],
+    Kaliop\eZMigrationBundle\eZMigrationBundle::class => ['all' => true],
 ];
